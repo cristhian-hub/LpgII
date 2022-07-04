abb4d2d5acac18ff46dcd1f5d3ed84ac53f270d4
Cristhian Helber Appi <cristhian.helber@hotmail.com>
2022-07-03 21:56:55 -0300

    1

---
+++ application/controllers/user.php
@@ -0,0 +1,27 @@
+<?php
+
+use Application\core\Controller;
+
+class User extends Controller
+{
+    
+  public function index()
+  {
+    $Users = $this->model('Users'); // é retornado o model Users()
+    $data = $Users::findAll();
+    $this->view('user/index', ['users' => $data]);
+  }
+
+@param  int 
+
+  public function show($id = null)
+  {
+    if (is_numeric($id)) {
+      $Users = $this->model('Users');
+      $data = $Users::findById($id);
+      $this->view('user/show', ['user' => $data]);
+    } else {
+      $this->pageNotFound();
+    }
+  }
+}
